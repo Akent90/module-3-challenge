381a233a94c18a291284fcaf0b1ec575f0a93f6b
akent <alexanderkent099@gmail.com>
2023-12-04 15:20:09 -0500

    Add user prompts for selecting password character types

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ function generatePassword() {
   var upperCaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '~', '`', '|', '}', '{', '[', ']', ':', ';', '?', '>', '<', ',', '.', '/', '-', '='];
+
+  var includeLowerCase = prompt("Include lowercase letters? (yes/no)");
+  var includeUpperCase = prompt("Include uppercase letters? (yes/no)");
+  var includeNumeric = prompt("Include numbers? (yes/no)");
+  var includeSpecialChars = prompt("Include special characters? (yes/no)");
+  var isValidChoice = (includeLowerCase === 'yes' || includeUpperCase === 'yes' || includeNumeric === 'yes' || includeSpecialChars === 'yes');
+
+  if (!isValidChoice) {
+    alert("You must select at least one character type.");
+    return "Invalid selection. Please try again.";
+  }
 }
 
 var generateBtn = document.querySelector("#generate");
